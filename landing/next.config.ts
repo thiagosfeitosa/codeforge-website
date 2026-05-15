a2a4aebe00edf60c9722e96b45ba8c7ec50a95f9
Thiago Feitosa <expert143780@reveloexperts.com>
2026-05-15 01:05:07 -0300

    Use static robots and sitemap for export

--- landing/next.config.ts
+++ landing/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -9,6 +10,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  output: "export",
 };
 
 export default nextConfig;
